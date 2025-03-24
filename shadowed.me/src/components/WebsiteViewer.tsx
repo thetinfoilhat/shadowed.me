f51de62262158c32be03b3b4eae42312ccee48a7
Arnav Sharma <46266106+ArnavSharma0904@users.noreply.github.com>
2025-03-24 17:05:36 -0500

    added in forced refresh

--- shadowed.me/src/components/WebsiteViewer.tsx
+++ shadowed.me/src/components/WebsiteViewer.tsx
@@ -403,6 +403,10 @@ export default function WebsiteViewer({ website, isEditor, onDelete }: WebsiteVi
               href="/jamboree"
               className="flex items-center font-medium hover:underline"
               style={{ color: primaryColor }}
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.href = '/jamboree';
+              }}
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
