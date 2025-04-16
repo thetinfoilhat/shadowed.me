523cc669e82e53c8186c6e51cdb7e95efac0197a
thetinfoilhat <adrianxie00@gmail.com>
2025-04-15 20:08:32 -0500

    favicon stuff excel renamed to sheets

--- shadowed.me/src/components/WebsiteEditor.tsx
+++ shadowed.me/src/components/WebsiteEditor.tsx
@@ -2444,7 +2444,7 @@ export default function WebsiteEditor({ website, onSave, isNew = false }: Websit
                           // Copy to clipboard
                           navigator.clipboard.writeText(dataWithHeaders)
                             .then(() => {
-                              toast.success('Copied to clipboard! Paste into Excel.');
+                              toast.success('Copied to clipboard! Paste into Sheets.');
                             })
                             .catch(err => {
                               console.error('Failed to copy:', err);
@@ -2456,7 +2456,7 @@ export default function WebsiteEditor({ website, onSave, isNew = false }: Websit
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
                         </svg>
-                        Copy for Excel
+                        Copy for Sheets
                       </button>
                       <button
                         onClick={() => {
